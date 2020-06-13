44545e3fd053df1e86396ef90e953aadcc8f5294
Emily Braman <embraman10@gmail.com>
2020-06-13 18:45:28 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,37 +1,40 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  dogs= ["shiba", "corgi", "lab", "pitbull"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  cats= ["artemis", "lady", "violet", "lilly"]
+  cats<< "kato"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  dogs= dogs= ["shiba", "corgi", "lab", "pitbull"]
+  dogs.unshift ("collie")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  cats= [artemis, lady, violet, lilly]
+  .pop(lilly)
 end
 
 def remove_element_from_start_of_array(array)
-  
+
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+
 end
 
 def retrieve_first_element_from_array(array)
-  
+
 end
 
 def retrieve_last_element_from_array(array)
-  
+
 end
 
 def update_element_from_index(array, index_number, element)
