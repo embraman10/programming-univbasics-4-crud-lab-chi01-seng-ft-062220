1d4677e26becaa6d246e3d63f6ac0a07e4cc7fb1
Emily Braman <embraman10@gmail.com>
2020-06-13 18:51:12 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,7 +12,7 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  dogs= dogs= ["shiba", "corgi", "lab", "pitbull"]
+  dogs= ["shiba", "corgi", "lab", "pitbull"]
   dogs.unshift ("wow")
 end
 
