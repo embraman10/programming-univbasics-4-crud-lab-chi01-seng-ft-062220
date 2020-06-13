70b17b6df407f49564ba69a5f4d6e808f94eeb8c
Emily Braman <embraman10@gmail.com>
2020-06-13 18:50:08 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
   cats= ["artemis", "lady", "violet", "lilly"]
-  lilly_ cat=cats.pop
+  lilly_cat= cats.pop
 end
 
 def remove_element_from_start_of_array(array)
