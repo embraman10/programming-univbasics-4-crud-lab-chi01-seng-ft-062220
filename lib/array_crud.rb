e585eac26d2dec52d7fb45076a9ecfa56f2303a4
Emily Braman <embraman10@gmail.com>
2020-06-13 19:01:34 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -42,5 +42,7 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
-  
+  ruby= ["wow", "boom", "arrays!", "bam!", "killer"]
+  ruby[4]= ["wow", "boom", "arrays!", "bam!", "killer"]
+  => "totally"
 end
